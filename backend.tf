6fcbfd1b843def0cf2df833945b550e8143ed094
herlessi <herlessi@gmail.com>
2025-10-05 19:09:08 -0300

    ajustes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "techchallenge-fiap-fase3-2153"
-    key    = "techchallenge/fiap/fase3/terraform.tfstate"
+    key    = "techchallenge/fiap/fase3/rds/terraform.tfstate"
     region = "us-east-1"
   }
 }
