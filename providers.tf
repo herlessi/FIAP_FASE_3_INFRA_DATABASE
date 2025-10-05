94dcc0db7c97db9e805dbc205ff9ea39251bbfa8
herlessi <herlessi@gmail.com>
2025-10-05 20:37:45 -0300

    aaa

--- providers.tf
+++ providers.tf
@@ -25,13 +25,39 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.auth.token
   load_config_file       = false
-  apply_retry_count      = 3
-  config_path            = "~/.kube/config"
-  # config_path            = data.aws_eks_cluster.cluster.kubeconfig_path
+  apply_retry_count      = 5
+  
+  # Configurações para melhor compatibilidade com CI/CD
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name,
+      "--region",
+      var.aws_region
+    ]
+  }
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.auth.token
+  
+  # Configuração alternativa de autenticação para CI/CD
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name,
+      "--region",
+      var.aws_region
+    ]
+  }
 }
