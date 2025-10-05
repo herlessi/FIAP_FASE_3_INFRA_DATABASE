2b526c0cd718f51558ba289cf8197368357ac47d
herlessi <herlessi@gmail.com>
2025-10-05 16:55:09 -0300

    ajustes

--- aws_secrets.tf
+++ aws_secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "rds-db-credentials3"
+  name = "rds-db-credentials4"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
